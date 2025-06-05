86498f7b224eb8d17580a0746be578e8353b66d9
Samarth-1206 <pawar06samarth14@gmail.com>
2025-06-05 19:17:48 +0530

    Update userbot.py

--- PURVIMUSIC/core/userbot.py
+++ PURVIMUSIC/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("PURVI_SUPPORT")
-                await self.one.join_chat("PURVI_UPDATES")
+                await self.one.join_chat("KAISEN_UPDATES")
+                await self.one.join_chat("GHOULS_SUPPORT")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("PURVI_SUPPORT")
-                await self.one.join_chat("PURVI_UPDATES")
+                await self.two.join_chat("KAISEN_UPDATES")
+                await self.one.join_chat("GHOULS_SUPPORT")
             except:
                 pass
             assistants.append(2)
@@ -93,7 +93,7 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("PURVI_SUPPORT")
+                await self.three.join_chat("KAISEN_UPDATES")
                 await self.one.join_chat("PURVI_UPDATES")
             except:
                 pass
@@ -115,7 +115,7 @@ class Userbot(Client):
             await self.four.start()
             try:
                 await self.four.join_chat("PURVI_SUPPORT")
-                await self.one.join_chat("PURVI_UPDATES")
+                await self.one.join_chat("GHOULS_SUPPORT")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("PURVI_SUPPORT")
-                await self.one.join_chat("PURVI_UPDATES")
+                await self.five.join_chat("KAISEN_UPDATES")
+                await self.one.join_chat("GHOULS_SUPPORT")
             except:
                 pass
             assistants.append(5)
